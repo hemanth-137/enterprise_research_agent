4ac4d58313c8beb9dd3147483259a16b08e1b94f
Astro Byte <sunny137hv@gmail.com>
2026-08-22 16:21:43 +0530

    Return context as string

--- retrieval/re_ranker.py
+++ retrieval/re_ranker.py
@@ -1,49 +1,70 @@
+import os
+os.environ["HF_HUB_OFFLINE"] = "1"
+
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-from vector_search import create_query_embeddings, get_chunks
+from .vector_search import create_query_embeddings, get_chunks
+
+def get_context(query):
+
+    model_name = "BAAI/bge-reranker-base"
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+    model = model.to("cuda")
+    model.eval()
 
 
-model_name = "BAAI/bge-reranker-base"
+    # query = """What planning application and development are being discussed, 
+    # what change did the commenter make from their 
+    # previous position, and what local reason did they give for supporting approval?"""
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    query_embedd = create_query_embeddings(query)
+    result_txt = get_chunks(query_embedd)
 
-model = model.to("cuda")
-model.eval()
+    pairs = [[query,txt] for _,txt in result_txt]
+
+    with torch.no_grad():
+        inputs = tokenizer(
+            pairs,
+            padding=True,
+            truncation=True,
+            return_tensors="pt",
+            max_length=512
+        ).to("cuda")
+
+        scores = model(**inputs, return_dict=True).logits.view(-1).float().cpu().tolist()
+
+    ranked = sorted(
+        zip(scores,result_txt),
+        key= lambda x:x[0],
+        reverse=True
+    )
+
+    results = []
+
+    for score,(meta,txt) in ranked[:3]:
+        #print(f"Score: {score}\n")
+        # print(f"doc_name : {meta.get('doc_name')}\n")
+        # print(f"pg_no : {meta.get('page_no')}\n")
+        # print(txt)
+        # print("\n")
+        # print("="*60)
+        results.append([txt,meta])
+
+    context = ""
+
+    for i,(txt,meta) in enumerate(results,start=1):
+        temp = f"Source {i}:\nContext: {txt}\nMetadata: {meta}\n\n---\n\n"
+        context += temp
+
+    return context
+
+if __name__ == "__main__":
+    pass
 
 
-query = """What planning application and development are being discussed, 
-what change did the commenter make from their 
-previous position, and what local reason did they give for supporting approval?"""
-
-query_embedd = create_query_embeddings(query)
-result_txt = get_chunks(query_embedd)
-
-pairs = [[query,txt] for _,txt in result_txt]
-
-with torch.no_grad():
-    inputs = tokenizer(
-        pairs,
-        padding=True,
-        truncation=True,
-        return_tensors="pt",
-        max_length=512
-    ).to("cuda")
-
-    scores = model(**inputs, return_dict=True).logits.view(-1).float().cpu().tolist()
-
-ranked = sorted(
-    zip(scores,result_txt),
-    key= lambda x:x[0],
-    reverse=True
-)
 
 
-for score,(meta,txt) in ranked[:5]:
-    print(f"Score: {score}\n")
-    print(f"doc_name : {meta.get('doc_name')}\n")
-    print(f"pg_no : {meta.get('page_no')}\n")
-    print(txt)
-    print("\n")
-    print("="*60)
