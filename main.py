2c731255680ab3beb6ccf2344aac4cbed5b413a8
Astro Byte <sunny137hv@gmail.com>
2026-09-01 22:02:38 +0530

    Include Sub Query generation and chat history

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
+from typing import List
+from pydantic import BaseModel, Field
 
-from generation.test_app import run_rag, QueryRequest
+from generation.generate_answer import run_rag
 
 app = FastAPI()
 
 DB_NAME = "embedd_test_collection"
 
+
+class ChatMessage(BaseModel):
+    role: str
+    content: str
+class QueryRequest(BaseModel):
+    query: str
+    chat_history: List[ChatMessage] = Field(default_factory=list)
+
+
 @app.post("/query")
 def query_endpoint(request: QueryRequest):
 
-    generator = run_rag(request, db_name=DB_NAME)
+    generator = run_rag(
+        request,
+        db_name=DB_NAME
+    )
 
     return StreamingResponse(
-        generator, 
+        generator,
         media_type="application/x-ndjson"
     )
+
 
 if __name__ == "__main__":
     #print("Starting on http://localhost:8000")
